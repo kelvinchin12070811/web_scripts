4976f8660a2375ee7252f64917b12a2f29869238
Kelvin Chin <chinkaiwen@yahoo.com>
2019-02-20 23:47:46 +0800

    Fixed some lag on navBar stick to the top

--- public/master.js
+++ public/master.js
@@ -10,16 +10,26 @@ function onMenuClicked()
 
 var navBar = null;
 var sticky = null;
+var siteHeader = null;
+var siteNav = null;
 
 function windowScrolled()
 {
     if (navBar == null) navBar = document.getElementById('mainNav');
     if (sticky == null) sticky = navBar.offsetTop;
+    if (siteHeader == null) siteHeader = document.getElementById('siteHeader');
+    if (siteNav == null) siteNav = document.getElementById('mainNav');
 
     if (window.pageYOffset >= sticky)
+    {
+        siteHeader.style.paddingBottom = siteNav.offsetBottom + 'px';
         navBar.classList.add('Sticky');
+    }
     else
+    {
+        siteHeader.style.paddingBottom = '';
         navBar.classList.remove('Sticky');
+    }
 }
 
 window.onscroll = function() { windowScrolled(); }
