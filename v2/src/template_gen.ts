b291599d295bc85ab5b043714afe930c0a270a63
Kelvin Chin <chinkaiwen@yahoo.com>
2020-03-30 15:42:47 +0800

    compleated responsive navbar

--- v2/src/template_gen.ts
+++ v2/src/template_gen.ts
@@ -12,16 +12,30 @@ function templateApplyBluePrint(): void
     let body = document.body;
     body.innerHTML = `<header w3-include-html="static/template/header.html"></header>
 <div class="w3-bar w3-black" id="main-nav-placeholder">
-    <nav class="content-limiter" id="main-nav">
-        <a class="w3-button w3-black w3-hover-white main-nav-btn" w3-repeat="nav" href="{{link}}">
+<div class="content-limiter">
+    <a class="w3-button w3-black w3-hover-white" id="show-nav" onclick="templateToggleShow()">
+        <i class="fas fa-bars"></i>
+    </a>
+    <nav id="main-nav" class="hide-nav">
+        <a class="w3-button w3-black w3-hover-white main-nav-btn"
+            w3-repeat="nav" href="{{link}}">
             {{name}}
         </a>
     </nav>
+</div>
 </div>
 <div class="content-limiter">
     ${body.innerHTML}
 </div>
 <footer w3-include-html="static/template/footer.html"></footer>`;
+    let article = body.getElementsByTagName("article")[0];
+    w3.addClass(article, "w3-card w3-padding w3-section w3-white");
+    w3.hide("#colapse-nav");
+}
+
+function templateToggleShow(): void
+{
+    w3.toggleClass("#main-nav", "hide-nav");
 }
 
 function templateGenPlaceHolders(): void
